2a6a1df538eed367066bc7e17c21ca5f2dfc4d80
w4odoo15 <147035907+w4odoo15@users.noreply.github.com>
2025-03-21 12:43:37 +0000

    Update __manifest__.py

--- w4_qr_banner/__manifest__.py
+++ w4_qr_banner/__manifest__.py
@@ -12,19 +12,14 @@
     'author': "W4 Services AG",
     'website': "https://just-odoo.agency/",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Accounting',
     'price': '450',
     'currency': 'EUR',
     'license': 'OPL-1',
     'version': '17.0.1.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','account', 'l10n_ch'],
 
-    # always loaded
     'data': [
         'views/company.xml',
         'views/qr_template.xml',
